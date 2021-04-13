8d2df929cc307934e44133b22e7c77efa1d7f61b
Mu-Monki <francismiguelgarcia72@gmail.com>
2021-04-13 13:09:32 +0800

    created more auth functions

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,4 +1,5 @@
 import Amplify, { Auth } from 'aws-amplify';
+import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
 import { config } from '../config';
 
 // Configure AWS Amplify
@@ -11,7 +12,10 @@ Amplify.configure({
     }
 });
 
+
+
 export class AuthService {
+
     static async getUserDetails() {
         return await Auth.currentAuthenticatedUser();
     }
@@ -50,10 +54,89 @@ export class AuthService {
     }
 
     static async confirmSignUp(email, code) {
+        // Confirmation of Email
         try {
-            await Auth.confirmSignUp(email, code);
+            await Auth.confirmSignUp(email, code)
+                .then(data => {
+                    if(data) {
+                        console.log('account confirmation success', data);
+                    }
+                });
         } catch (error) {
             console.log('error confirming sign up', error);
+        }
+    }
+
+    static async createCognitoUser(email, tempPassword, args) {
+        // Creates AWS Cognito User
+        try {
+            console.log('data', { email, tempPassword });
+            const { user } = await Auth.signUp({
+                username: email,
+                password: tempPassword,
+            });
+
+            console.log('created cognito user!', user);
+            return user;
+        } catch (error) {
+            console.log('cognito error signing up:', error);
+        }
+    }
+
+    static async disableCognitoUser(email) {
+
+        // AWS JS SDK Cognito Identity Provider Configuration
+        const cognitoIdentityProvider = new CognitoIdentityProvider({
+            region: config.cognito.REGION,
+        });
+
+        // Disables AWS Cognito User
+        const params = { 
+            UserPoolId: config.cognito.USER_POOL_ID,
+            Username: email
+        };
+
+
+        const data = await cognitoIdentityProvider.adminDisableUser(params)
+                    .then((result) => {
+                        console.log('user disabled', result);
+                        return result;
+                    })
+                    .catch((err) => {
+                        console.log('error disabling', err);
+                    })
+        if(data) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static async deleteCognitoUser(email) {
+        // AWS JS SDK Cognito Identity Provider Configuration
+        const cognitoIdentityProvider = new CognitoIdentityProvider({
+            region: config.cognito.REGION,
+        });
+
+        // Deletes AWS Cognito User
+        const params = {
+            UserPoolId: config.cognito.USER_POOL_ID,
+            Username: email
+        };
+        
+        const data = await cognitoIdentityProvider.adminDeleteUser(params)
+                .then((result) => {
+                    console.log('user deleted', result);
+                    return result;
+                })
+                .catch((err) => {
+                    console.log('error deleted', err);
+                });
+                
+        if(data) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
