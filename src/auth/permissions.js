78adf70186d33df231b629874c8dcfd4de549861
Mu-Monki <francismiguelgarcia72@gmail.com>
2021-04-13 13:09:12 +0800

    user crud premissions

--- src/auth/permissions.js
+++ src/auth/permissions.js
@@ -1,17 +1,22 @@
 import { rule, shield, and, or, not } from 'graphql-shield';
-import { isAuthenticated, isAdmin, isTeamLeader, isRosterManager, isSupportWorker } from './rules';
+import { isAuthenticated, isAdmin, isTeamLeader, isRosterManager, isSupportWorker, isGuest } from './rules';
 
 // Permissions
 const permissions = shield({
     Query: {
         users: and(isAuthenticated),
+
+        // Queries for testing service authorization
         onlyForAdmin: and(isAuthenticated, isAdmin),
         onlyForTeamLeaders: and(isAuthenticated, isTeamLeader),
         onlyForRosterManagers: and(isAuthenticated, isRosterManager),
         onlyForSupportWorkers: and(isAuthenticated, isSupportWorker),
     },
     Mutation: {
-        // createUser: and(isAuthenticated, isAdmin),
+        createUser: and(isAuthenticated, isAdmin),
+        disableUser: and(isGuest),
+        deleteUser: and(isGuest),
+        confirmSignup: and(isGuest),
     },
     User: isAuthenticated,
 });
