1c27db82bac34db12cc981316eca748f83bdd079
Mu-Monki <francismiguelgarcia72@gmail.com>
2021-04-20 19:13:34 +0800

    added user options file

---
+++ src/constants/users.js
@@ -0,0 +1,19 @@
+export const USER_OPTIONS = {
+    // ACCOUNT STATUS OPTIONS
+    ACCOUNT_STATUSES: {
+        ACTIVE: 'Active',
+        INACTIVE: 'Inactive',
+        DISABLED: 'Disabled',
+        FORCE_CHANGE: 'Force Change',
+        RESIGNED: 'Resigned',
+        TERMINATED: 'Terminated',
+    },
+
+    // USER ROLES (Cognito Groups)
+    ROLES: {
+        ADMIN: 'Admin',
+        MANAGER: 'Manager',
+        TEAM_LEADER: 'TeamLead',
+        SUPPORT_WORKER: 'SupportWorker',
+    },
+};
